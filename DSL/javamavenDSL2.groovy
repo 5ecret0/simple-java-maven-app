852bc74e1a990e32db45115fd93edc94b30e3e21
5ecret0 <86318498+5ecret0@users.noreply.github.com>
2023-05-09 21:48:51 +0200

    Update javamavenDSL2.groovy

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,18 +1,18 @@
 job('Java Maven App DSL 2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/5ecret0/simple-java-maven-app.git', 'main') { node ->
+            node / gitConfigName('secreto')
+            node / gitConfigEmail('cristianseco12@gmail.com')
         }
     }
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('maven_jenkins')
           goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
+          mavenInstallation('maven_jenkins')
           goals('test')
         }
         shell('''
